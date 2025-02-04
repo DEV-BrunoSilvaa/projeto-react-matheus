2debc61f7a677bd9636093e9fe5a39de7a376923
DEV-Bruno <brunosurf1005@hotmail.com>
2025-02-04 01:01:54 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const name = 'Matheus'
+
+  const newName = name.toUpperCase()
+
+  function sum (a, b) {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
+  return (
+    <div className='App'>
+      <h2>alterando JSX</h2>
+      <p> ola, {newName}</p>
+      <p> soma: {sum(1,2)} </p>
+      <img src={url} alt='minha imagem'></img>
+    </div>
+  );
+}
+
+export default App;
